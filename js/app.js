0069fe5fbfb774d621e56e0d727613783767e67e
Anish <ak@kryptosmobile.com>
2016-08-03 18:09:38 +0530

    push research

--- js/app.js
+++ js/app.js
@@ -144,7 +144,7 @@ var MyCampusApp = {
                 $.jStorage.set('tenant', tenantid);
                 storedMetadata = data;
 
-                if(window.device && data.pushconfig) {
+                if(window.device) {
                     MyCampusApp.activatePushNotification(tenantid, data.pushconfig);
                 }
                // var message = '<div style="margin: 2px; vertical-align: middle; display: inline-block"><i class="icon-cog icon-spin icon-4x"></i><h3 style="color:white;">Initializing..</h3></div>';
@@ -852,6 +852,119 @@ var MyCampusApp = {
 
     activatePushNotification : function(tenantId, pushconfig) {
         try {
+			var pushNotification;
+			pushNotification = window.plugins.pushNotification;
+			alert ("Called activate push");
+			window.onNotification = function(e) {
+				alert ("Event received : " + e.event);
+				switch( e.event )
+				    {
+				    case 'registered':
+				        if ( e.regid.length > 0 )
+				        {
+				            //$("#app-status-ul").append('<li>REGISTERED -> REGID:' + e.regid + "</li>");
+				            alert ("Registered : " + e.regid);
+				            // Your GCM push server needs to know the regID before it can push to this device
+				            // here is where you might want to send it the regID for later use.
+				            console.log("regID = " + e.regid);
+				        }
+				    break;
+
+				    case 'message':
+				        // if this flag is set, this notification happened while we were in the foreground.
+				        // you might want to play a sound to get the user's attention, throw up a dialog, etc.
+				        if ( e.foreground )
+				        {
+				            //$("#app-status-ul").append('<li>--INLINE NOTIFICATION--' + '</li>');
+				            alert ("Foreground : notification received " + e);
+
+				            // on Android soundname is outside the payload.
+				            // On Amazon FireOS all custom attributes are contained within payload
+				            //var soundfile = e.soundname || e.payload.sound;
+				            // if the notification contains a soundname, play it.
+				            //var my_media = new Media("/android_asset/www/"+ soundfile);
+				            //my_media.play();
+				        }
+				        else
+				        {  // otherwise we were launched because the user touched a notification in the notification tray.
+				            if ( e.coldstart )
+				            {
+				                //$("#app-status-ul").append('<li>--COLDSTART NOTIFICATION--' + '</li>');
+				                alert ("Cold start notification : " + e);
+				            }
+				            else
+				            {
+				                //$("#app-status-ul").append('<li>--BACKGROUND NOTIFICATION--' + '</li>');
+				                alert ("Background notification : " + e);
+				            }
+				        }
+
+				       //$("#app-status-ul").append('<li>MESSAGE -> MSG: ' + e.payload.message + '</li>');
+				       alert ("Message : " + e.payload.message);
+				           //Only works for GCM
+				       //$("#app-status-ul").append('<li>MESSAGE -> MSGCNT: ' + e.payload.msgcnt + '</li>');
+				       alert ("Message count : " + e.payload.msgcnt);
+				       //Only works on Amazon Fire OS
+				       //$status.append('<li>MESSAGE -> TIME: ' + e.payload.timeStamp + '</li>');
+				    break;
+
+				    case 'error':
+				        //$("#app-status-ul").append('<li>ERROR -> MSG:' + e.msg + '</li>');
+				        alert ("ERROR MSG : " + e.msg);
+				    break;
+
+				    default:
+				        //$("#app-status-ul").append('<li>EVENT -> Unknown, an event was received and we do not know what it is</li>');
+				        alert ("Unknown, an event was received and we do not know what it is");
+				    break;
+  					}
+			}
+			function successHandler (result) {
+			    alert('Success handler result = ' + result);
+			}
+			function errorHandler (error) {
+			    alert('Error handler error = ' + error);
+			}
+			if ( device.platform == 'android' || device.platform == 'Android' || device.platform == "amazon-fireos" ){
+			    pushNotification.register(
+			    successHandler,
+			    errorHandler,
+			    {
+			        "senderID":"59818635520",
+			        "ecb":"onNotification"
+			    });
+			}/* else if ( device.platform == 'blackberry10'){
+			    pushNotification.register(
+			    successHandler,
+			    errorHandler,
+			    {
+			        invokeTargetId : "replace_with_invoke_target_id",
+			        appId: "replace_with_app_id",
+			        ppgUrl:"replace_with_ppg_url", //remove for BES pushes
+			        ecb: "pushNotificationHandler",
+			        simChangeCallback: replace_with_simChange_callback,
+			        pushTransportReadyCallback: replace_with_pushTransportReady_callback,
+			        launchApplicationOnPush: true
+			    });
+			} else {
+			    pushNotification.register(
+			    tokenHandler,
+			    errorHandler,
+			    {
+			        "badge":"true",
+			        "sound":"true",
+			        "alert":"true",
+			        "ecb":"onNotificationAPN"
+			    });
+			}*/
+
+
+
+
+
+			if(true) {
+				return;
+			}
             var appId = pushconfig.ApplicationId;
             var clientKey = pushconfig.ClientKey;
             parsePlugin.initialize(appId, clientKey, function() {
@@ -878,11 +991,6 @@ var MyCampusApp = {
                 //alert('error');
             });
 
-            /*parsePlugin.unsubscribe('SampleChannel', function(msg) {
-             alert('OK');
-             }, function(e) {
-             alert('error');
-             });*/
         }catch(e) {
 
         }
